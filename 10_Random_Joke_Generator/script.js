997f5cf37ffeec89bb35ee4187f2e04f47b1038f
Spatil <sahilpatil67049@gmail.com>
2024-02-15 23:14:06 +0530

    Random Joke Generator

---
+++ 10_Random_Joke_Generator/script.js
@@ -0,0 +1,18 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,political,sexist,explicit&type=single";
+
+
+let getJoke = () =>{
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+    .then(data => data.json())
+    // .then(item => console.log(item.joke));
+    .then(item =>{
+        jokeContainer.textContent = `${item.joke}`
+        jokeContainer.classList.add("fade");
+    });
+}
+
+btn.addEventListener("click", getJoke);
+getJoke();
